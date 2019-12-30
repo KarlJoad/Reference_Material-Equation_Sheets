51a3d97412b036425ef1d10e33a494a118b73c07
Karl Hallsby <khallsby@hawk.iit.edu>
2019-12-30 13:44:12 +0100

    EDAP05: ADT Implementation and no Specification

---
+++ EDAP05-Concepts_Programming_Languages-Sections/Abstract_Data_Types/Code/IntVector_No_Spec.py
@@ -0,0 +1,13 @@
+IntVector = bool
+
+def create(len : int) -> IntVector:
+    return False
+
+def length(v : IntVector) -> int:
+    return 0
+
+def append(v : IntVector, value : int):
+    pass  # Return nothing
+
+def get (v : IntVector, offset : int) -> int:
+    return 0
