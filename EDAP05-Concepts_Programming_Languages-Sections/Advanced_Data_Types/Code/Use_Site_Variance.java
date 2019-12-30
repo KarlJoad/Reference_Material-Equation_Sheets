fcf08e21b0c36a0ae3d0d7d1d99ed735e1201f06
Karl Hallsby <khallsby@hawk.iit.edu>
2019-12-30 22:27:16 +0100

    EDAP05: Fixed a type in Use-Site Variance source code file

--- EDAP05-Concepts_Programming_Languages-Sections/Advanced_Data_Types/Code/Use_Site_Variance.java
+++ EDAP05-Concepts_Programming_Languages-Sections/Advanced_Data_Types/Code/Use_Site_Variance.java
@@ -1,4 +1,4 @@
 Box<? extends A> covariantBox = new Box<B>();
-Box<? super B> c ontravariantBox = new Box<A>();
+Box<? super B> contravariantBox = new Box<A>();
 Box<?> bivariantBox = new Box<A>();
 Box<A> invariantBox = new Box<A>();
