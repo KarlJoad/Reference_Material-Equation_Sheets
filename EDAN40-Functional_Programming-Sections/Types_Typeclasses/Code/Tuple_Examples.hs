f3abb3b93b881c9aba944bd5c9013c54d7e6f671
Karl Hallsby <khallsby@hawk.iit.edu>
2020-03-29 20:13:38 +0200

    EDAN40: Examples of tuples and their syntax

---
+++ EDAN40-Functional_Programming-Sections/Types_Typeclasses/Code/Tuple_Examples.hs
@@ -0,0 +1,4 @@
+(False, "True", False) -- :: (Bool, String, Bool)
+('a', (2^8976), 'c', 3.14) -- :: (Char, Integer, Char, Float)
+("One", "Two", "Three") -- :: (String, String, String)
+(1, "2", '3') -- :: (Int, String, Char)
