046583b284d0d8461c2468e5fc6f04b1cdc0bad2
Karl Hallsby <khallsby@hawk.iit.edu>
2020-03-30 19:26:41 +0200

    EDAN40: Example of extending a typeclass by extending Eq to Ord

---
+++ EDAN40-Functional_Programming-Sections/Declaring_Types/Code/Ord_Typeclass-Definition.hs
@@ -0,0 +1,8 @@
+class Eq a => Ord a where
+    (<), (<=), (>), (>=) :: a -> a -> Bool
+    min, max :: a -> a -> a
+
+    min x y | x <= y = x
+            | otherwise = y
+    max x y | x <= y = y
+            | otherwise x
