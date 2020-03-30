af3b4aa8e477469a822204e2a764855aac305ba5
Karl Hallsby <khallsby@hawk.iit.edu>
2020-03-30 18:37:47 +0200

    EDAN40: Extended example of using parameterized data declaration

---
+++ EDAN40-Functional_Programming-Sections/Declaring_Types/Code/Data_Declaration-Parameterized.hs
@@ -0,0 +1,10 @@
+data Maybe a = Nothing
+             | Just a
+
+safediv :: Int -> Int -> Maybe Int
+safediv _ 0 = Nothing
+safediv m n = Just (m `div` n)
+
+safehead :: [a] -> Maybe a
+safehead [] = Nothing
+safehead xs = Just (head xs)
