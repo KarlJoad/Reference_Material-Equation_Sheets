f25d58dabc8acec9fa9547d80ab7c4e2a032c4a1
Karl Hallsby <khallsby@hawk.iit.edu>
2020-04-04 14:31:08 +0200

    EDAF35: Basic syntax (in code) of a Monitor

---
+++ EDAF35-Operating_Systems-Sections/CPU_Scheduling_Synchronization/Code/Monitor_Type.c
@@ -0,0 +1,20 @@
+monitor name {
+	condition x;
+	condition y;
+	/* Shared Variable Declarations */
+	function P1 (...) {
+		...
+	}
+
+	function P2 (...) {
+		...
+	}
+
+	function Pn (...) {
+		...
+	}
+
+	initialization_code (...) {
+		...
+	}
+}
