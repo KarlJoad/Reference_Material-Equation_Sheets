97bb3ccaef4241643bb43fe8ac1fbbc61c4ddb08
Karl Hallsby <karl@hallsby.com>
2020-10-04 01:25:29 -0500

    C Primer: Add example of if-else if-else statement's syntax

---
+++ C_Primer-Sections/Syntax/Code/if-elseif-else-statement.c
@@ -0,0 +1,10 @@
+if (logical-predicate) {
+	this-predicate-true-clause;
+} else if (logical-predicate) {
+	this-predicate-true-clause;
+} else {
+	all-predicates-false-clause;
+}
+
+/* Note that the ( and ) MUST be present in the statement to denote the predicate.
+ * { and } can be omitted ONLY IF the clause has a SINGLE statement in it. */
