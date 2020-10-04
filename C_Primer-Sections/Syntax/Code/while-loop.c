682132d4d37fb5e87e4791ea2012a74585c238b9
Karl Hallsby <karl@hallsby.com>
2020-10-04 01:26:38 -0500

    C Primer: Add example of while loop's syntax

---
+++ C_Primer-Sections/Syntax/Code/while-loop.c
@@ -0,0 +1,8 @@
+while (condition) {
+	statements;
+}
+
+/* The body of the while-loop will continue to execute UNTIL condition becomes
+ * false.
+ * The ( and ) are NEEDED for the predicate. In addition, the { and } are
+ * NEEDED if there is more than one statement in the body.  */
