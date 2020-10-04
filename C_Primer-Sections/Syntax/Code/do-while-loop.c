00001d999e4c6da76f32ff730d1c5e4caa1e916f
Karl Hallsby <karl@hallsby.com>
2020-10-04 01:27:13 -0500

    C Primer: Add example of do while loop's syntax

---
+++ C_Primer-Sections/Syntax/Code/do-while-loop.c
@@ -0,0 +1,9 @@
+do {
+	statement;
+}
+while(condition);
+
+/* The { and } SHOULD ALWAYS be included for the body of a do-while loop, although
+ * the language and compiler do NOT require them. However, the ( and ) ARE required
+ * to surround the condition.
+ * In addition, the ending semicolon ; at the end of the while IS mandatory. */
