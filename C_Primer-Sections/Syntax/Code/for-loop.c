1fc0184de0c91fd864eac79313983f238ce8600f
Karl Hallsby <karl@hallsby.com>
2020-10-04 01:27:02 -0500

    C Primer: Add example of for loop's syntax

---
+++ C_Primer-Sections/Syntax/Code/for-loop.c
@@ -0,0 +1,9 @@
+for(init value; predicate; per-loop change) {
+	statement;
+}
+
+/* The body of the ( and ) part must be written that way, and MUST be in the
+ * parentheses.
+ * The { and } are MANDATORY if there is MORE than 1 statement in the for loop.
+ * The initial value MUST be countably changeable, meaning an integer or
+ * something else.*/
